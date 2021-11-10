d7a519edc3675cb45d88495bac2f18c4e36cfa5f
Balazs Kreith <kreith.balazs@gmail.com>
2021-11-10 11:59:37 +0200

    simplified myapp

--- observer-myapp/README.md
+++ observer-myapp/README.md
@@ -1,9 +1,11 @@
 
-Observer + Kafka + Custom NodeJS App + Prometheus + Grafana
+Observer + MyApp
 ===
 
-Setup an observer, which sends the generated reports to kafka. From kafka a custom NodeJS app reads the data calculate metrics and expose them to `/metrics` local endpoint.
-Prometheus scrapes `/metrics` target and updates its internal database. Finally Grafana reads Prometheus database and visualize metrics
+Setup an observer sends reports to a [socket-io](socket.io) sink.
+a custom NodeJS service, [myApp](./myApp/main.js) receives reports from the observer 
+and calculate the number of calls started, ended and durations to a prometheus 
+`/metrics` endpoint at myApp.
 
 ### Usage
 
@@ -11,16 +13,7 @@ Prometheus scrapes `/metrics` target and updates its internal database. Finally 
     docker-compose up 
 ```
 
-NOTE: It may happens that the kafka has not been initialized fully before the observer starts running and therefore the sink connection does not succeed. In those occasion retry or increase the initial waiting time for the observer 
-through an environment variable `INITIAL_WAITING_TIME_IN_S` you find it in the `docker-compose.yaml`
-
 
 ### Configurations
 
 Observer configuration files (`observer-config/config.yaml`) setup a sink to the kafka.
-
-
-### Additional notes:
-
- * if you want to customize any Kafka parameters, simply add them as environment variables in ```docker-compose.yml```.
- * Kafka's log4j usage can be customized by adding environment variables prefixed with ```LOG4J_```.
--- observer-myapp/myapp/main.js
+++ observer-myapp/myapp/main.js
@@ -1,53 +1,82 @@
-const { KafkaStreams } = require("kafka-streams");
-const kafkajsConfig = require("./kafkajs-config.js");
-const factory = new KafkaStreams(kafkajsConfig);
 const Prometheus = require('prom-client')
+
+const log4js = require('log4js');
+const moduleName = module.filename.slice(__filename.lastIndexOf("/")+1, module.filename.length -3);
+const logger = log4js.getLogger(moduleName);
+logger.level = "info";
 
 /** Define exportable metrics **/
 const startedCalls = new Prometheus.Counter({
     name: 'started_calls',
     help: 'The number of initiated calls by the clients',
-    labelNames: ['serviceId', 'mediaUnitId', 'roomId'],
+    labelNames: ['serviceId', 'roomId'],
 });
 
 const endedCalls = new Prometheus.Counter({
     name: 'ended_calls',
     help: 'The number of finished calls by the clients',
-    labelNames: ['serviceId', 'mediaUnitId', 'roomId'],
+    labelNames: ['serviceId', 'roomId'],
 });
 
-factory.getKStream()
-    .from("observertc-call-event")
-    .mapJSONConvenience()
-    .mapWrapKafkaValue()
-    .forEach(callEvent => {
-        if (!callEvent) {
-            return;
-        }
-        switch (callEvent.name) {
-            case "CALL_STARTED":
-                startedCalls
-                    .labels(callEvent.serviceId, callEvent.mediaUnitId, callEvent.roomId)
-                    .inc();
-            case "CALL_ENDED":
-                endedCalls
-                    .labels(callEvent.serviceId, callEvent.mediaUnitId, callEvent.roomId)
-                    .inc();
-        }
+const callDurations = new Prometheus.Histogram({
+    name: 'call_durations',
+    help: 'The distribution of duration of calls',
+    labelNames: ['serviceId'],
+    buckets: [
+        5 * 60 * 1000, // 5 min calls
+        17 * 60 * 1000, // 17 min calls (for 15 mins meeting)
+        32 * 60 * 1000, // 32 min calls (for the 30 mins meeting)
+        62 * 60 * 1000, // 62 min calls (for the 1h meetings)
+        5 * 60 * 60 * 1000, // 5h calls
+    ]
+});
+
+function main() {
+    const SERVER_PORT = 5080;
+    const app = require('express')();
+    const http = require('http').Server(app);
+    const io = require('socket.io')(http);
+
+    app.get('/metrics', function(request, response) {
+        const register = Prometheus.register;
+        response.setHeader('Content-Type', register.contentType)
+        register.metrics().then(content => {
+            response.end(content);
+        });
     });
+    
+    //Whenever someone connects this gets executed
+    io.on('connection', function(socket) {
+        const calls = new Map();
+        logger.info('Socket is connected');
 
-// conferences with 2 participants over time
+        // Report Types: https://github.com/ObserveRTC/schemas-2.0/tree/main/generated-schemas/reports/v3
+        socket.on("CALL_EVENT", data => {       
+            const buffer = Buffer.from(data);
+            const decodedString = String.fromCharCode.apply(null, buffer);
+            const callEvent = JSON.parse(decodedString);
+            const { name: eventType, serviceId, callId, timestamp } = callEvent;
 
-// Average conference size
+            // Type of call events: https://github.com/ObserveRTC/schemas-2.0/blob/main/generated-schemas/reports/v3/call-event-report.md
+            switch (eventType) {
+                case "CALL_STARTED":
+                    startedCalls.labels(serviceId).inc();
+                    calls.set(callId, timestamp);
+                    break;
+                case "CALL_ENDED":
+                    endedCalls.labels(serviceId).inc();
+                    const durationInMs = calls.has(callId) ? (timestamp - calls.delete(callId)) : -1;
+                    callDurations.observe(durationInMs);
+                    break;
+            }
+        });
+        socket.on('disconnect', function () {
+            logger.info("Socket is disconnected");
+        });
+    });
+    http.listen(SERVER_PORT, function() {
+        logger.info("Server is listening on " + SERVER_PORT);
+    });
+}
 
-// Number of unique users (unique cookie ids for a given time period)
-
-// Turn usage - % of joined sessions that used TURN
-
-// % of user joins with media issues over time
-
-// % of user joins (or join attempts) with connectivity issues over time
-
-// List of users with issues by issue with links to more detailed logs
-
-
+main();
